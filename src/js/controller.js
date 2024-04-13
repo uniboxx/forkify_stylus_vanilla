7bd52c3dccdaf52536f6e9ab19c68894b0869d0e
Davide Pedrotti <unibox@duck.com>
2024-04-13 16:43:13 +0200

    added MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -9,5 +10,27 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
+// https://forkify-api.herokuapp.com/api/v2/recipes
+
+// api key: ec77d6d1-2c1e-4084-ae99-004c0f3da9bd
 
 ///////////////////////////////////////
+async function controlRecipes() {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes),
+);
